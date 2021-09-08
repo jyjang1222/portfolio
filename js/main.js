06e53a689d58639329b1770a0505ba72b83d57f5
jyjang1222 <89000811+jyjang1222@users.noreply.github.com>
2021-09-08 23:53:55 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,17 @@ $(function() {
   $('.btn_header').click(function() {
     $('#header, .container').toggleClass('header_off')
   })
+
+  var progress = ['90', '90', '60', '70', '50' , '70']
+  
+  for(var i = 1; i <= 6; i++) {
+    $('.bar' + i).rProgressbar({
+      percentage: progress[i - 1],
+      fillBackgroundColor:'#0AF',
+      backgroundColor:'transparent',
+      borderRadius:'0 15px 15px 0',
+      height:'22px',
+      width:'244px'
+    })
+  }
 }) //ready
