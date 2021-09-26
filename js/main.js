813821e708def6f011a484339039201c7adc75bc
jyjang1222 <89000811+jyjang1222@users.noreply.github.com>
2021-09-26 22:59:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('html, body').animate({ scrollTop: 0 }, 'slow'); 
+  $('html, body').animate({ scrollTop: 0 }, 'slow');
 
   $('.btn_header').click(function() {
     $('#header, .container').toggleClass('header_off')
@@ -68,47 +68,4 @@ $(function() {
     
     $('html').stop().animate({scrollTop: menu[menu_idx]}, 800);
   })//GNB클릭 이벤트
-
-
-  var elm = ".sec";
-
-  $(elm).each(function (index) {
-      // 개별적으로 Wheel 이벤트 적용
-      $(this).on("mousewheel DOMMouseScroll", function (e) {
-          e.preventDefault();
-          var delta = 0;
-          if (!event) event = window.event;
-          if (event.wheelDelta) {
-              delta = event.wheelDelta / 120;
-              if (window.opera) delta = -delta;
-          } 
-          else if (event.detail)
-              delta = -event.detail / 3;
-          var moveTop = $(window).scrollTop();
-          var elmSelecter = $(elm).eq(index);
-          // 마우스휠을 위에서 아래로
-          if (delta < 0) {
-              if ($(elmSelecter).next() != undefined) {
-                  try{
-                      moveTop = $(elmSelecter).next().offset().top;
-                  }catch(e){}
-              }
-          // 마우스휠을 아래에서 위로
-          } else {
-              if ($(elmSelecter).prev() != undefined) {
-                  try{
-                      moveTop = $(elmSelecter).prev().offset().top;
-                  }catch(e){}
-              }
-          }
-            
-          // 화면 이동 0.8초(800)
-          $("html,body").stop().animate({
-              scrollTop: moveTop + 'px'
-          }, {
-              duration: 800, complete: function () {
-              }
-          });
-      });
-  }); //http://2nusa.blogspot.com/2016/10/jquery-mouse-wheel.html 마우스휠 소스 적용
 }) //ready
